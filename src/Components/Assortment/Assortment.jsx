cb5124cc31d07020cad1e5832f5263fd6426d27d
gamecatt <kotekotov@gmail.com>
2023-06-10 11:32:51 +0300

    assortment update

--- src/Components/Assortment/Assortment.jsx
+++ src/Components/Assortment/Assortment.jsx
@@ -6,9 +6,17 @@ import {ProductCard} from "./ProductCard/ProductCard.jsx";
 function Assortment() {
   return (
     <div className="assortment">
+      <div className="products">
       {jsonData.map((item) => (
         <ProductCard key={item.id} data={item} />
       ))}
+      </div>
+      <div className="action-panel">
+        <div className="some-btn">icon-cart</div>
+        <div className="some-btn">icon-likes</div>
+        <div className="some-btn">icon-profile</div>
+        <div className="some-btn">icon-discount</div>
+      </div>
     </div>
   );
 }
